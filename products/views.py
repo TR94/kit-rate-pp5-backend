1be5fecc71edb6b574425062cc4ee7d1c8d0f5b2
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-25 20:59:33 +0000

    Add ProductList view and define get method

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+# Taken from Code Institute lesson material on Django REST Framework
+class ProductList(APIView):
+    # View to list all products
+
+    # Method to get products list and serialize to JSON
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True, context={'request': request})
+        return Response(serializer.data)
+
