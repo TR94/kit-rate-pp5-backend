1ade65011637e6f9b0a9ae0d1fdf9f2e3d371f15
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-31 13:27:42 +0000

    Add reviews views and urls

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from kitrate_api.permissions import IsOwnerOrReadOnly
+from .models import Review
+from .serializers import ReviewSerializer, ReviewDetailSerializer
 
-# Create your views here.
+class ReviewList(generics.ListCreateAPIView):
+# Django generics to create the GET and PUT methods
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+
+    def perform_create(self, serializer):
+        # this associates a user with the review
+        serializer.save(owner=self.request.user)
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    # Django generics takes care of GET, PUT, DELETE methods
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReviewDetailSerializer
+    queryset = Review.objects.all()
