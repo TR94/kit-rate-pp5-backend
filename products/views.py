0dddda2ce017cb334721d5667c1d5568f0dbc1ad
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-06 12:32:59 +0000

    Refactored Profile view and Product view with django generics

--- products/views.py
+++ products/views.py
@@ -1,7 +1,4 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response 
+from rest_framework import generics, permissions
 from .models import Product
 from .serializers import ProductSerializer
 from kitrate_api.permissions import IsOwnerOrReadOnly
@@ -9,33 +6,24 @@ from kitrate_api.permissions import IsOwnerOrReadOnly
 
 # Based on Code Institute lesson material on Django REST Framework
 
-class ProductList(APIView):
+class ProductList(generics.ListCreateAPIView):
     # View to list all products
-
-    # Setting the serializer class automatically creates a form for the "p" method
+    # Django generics takes care of GET and PUT methods
+    
+    # Setting the serializer class automatically creates a form for the "put" method
     serializer_class = ProductSerializer
 
     # Permissions to only allow logged in users to create a product
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    # Method to get products list and serialize to JSON
-    def get(self, request):
-        products = Product.objects.all().select_related('category')
-        serializer = ProductSerializer(products, many=True, context={'request': request})
-        return Response(serializer.data)
+    queryset = Product.objects.all()
 
-    # Method to create a new product and save it to database
-    def put(self, request):
-        serializer = ProductSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-class ProductDetail(APIView):
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     # View to see the details of a specific product by ID
+    # Django generics takes care of GET, PUT and DELETE methods
 
     # Setting the serializer class automatically creates a form for the "put" method
     serializer_class = ProductSerializer
@@ -43,34 +31,7 @@ class ProductDetail(APIView):
     # Setting the permission class to allow only data owners to update their data
     permission_classes = [IsOwnerOrReadOnly]
 
-    # Method to see if specific product is avaialble, by primary key
-    def get_object(self, pk):
-        try:
-            product = Product.objects.get(pk=pk)
-            self.check_object_permissions(self.request, product)
-            return product
-        except Product.DoesNotExist:
-            raise Http404
+    queryset = Product.objects.all()
 
-    # Method to get the specific product using get_ojbect method and serialize to JSON
-    def get(self, request, pk):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, context={'request': request})
-        return Response(serializer.data)
-
-    # Method to retrieve a specific product, update details through serializer and save
-    def put(self, request, pk):
-        product = self.get_object(pk)
-        serializer = ProductSerializer(product, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-    # Method to delete a specific product 
-    def delete(self, request, pk):
-        product = self.get_object(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,4 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response 
+from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from kitrate_api.permissions import IsOwnerOrReadOnly
@@ -9,19 +6,19 @@ from kitrate_api.permissions import IsOwnerOrReadOnly
 
 # Based on Code Institute lesson material on Django REST Framework
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     # View to list all profiles
+    # Django generics takes care of GET method
     # Profile creation is handled by Django signals - profiles > models.py
 
-    # Method to get profiles list and serialize to JSON
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
-        return Response(serializer.data)
+    # Setting the serializer class automatically creates a form for the "put" method
+    serializer_class = ProfileSerializer
 
+    queryset = Profile.objects.all()
 
-class ProfileDetail(APIView):
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     # View to see the details of a specific profile by ID
+    # Django generics takes care of GET and PUT methods
 
     # Setting the serializer class automatically creates a form for the "put" method
     serializer_class = ProfileSerializer
@@ -29,28 +26,7 @@ class ProfileDetail(APIView):
     # Setting the permission class to allow only data owners to update their data
     permission_classes = [IsOwnerOrReadOnly]
 
-    # Method to see if specific profile is avaialble, by primary key
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+    queryset = Profile.objects.all()
 
-    # Method to get the specific profile using get_ojbect method and serialize to JSON
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    # Method to retrieve a specific profile, update details through serializer and save
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
 
