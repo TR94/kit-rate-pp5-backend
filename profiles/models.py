0e35a610bede47327a5363658dabce620de2382c
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-11 11:11:01 +0000

    Update image path of profile > models.py

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', 
-        default='../default_profile_k1hygn'
+        default='../default_profile_iwm3kr'
     )
 
     class Meta:
