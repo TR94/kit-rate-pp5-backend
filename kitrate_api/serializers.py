6ac8a59b90c384bf5978ac92de735ec1c740cf52
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-11 14:38:21 +0000

    Removed profile_username from kitrate_api > serializers.py

--- kitrate_api/serializers.py
+++ kitrate_api/serializers.py
@@ -4,10 +4,9 @@ from rest_framework import serializers
 # Code taken from dj-rest-auth documentation
 class CurrentUserSerializer(UserDetailsSerializer):
     profile_id = serializers.ReadOnlyField(source='profile.id')
-    profile_username = serializers.ReadOnlyField(source='user.username')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id','profile_username', 'profile_image'
+            'profile_id', 'profile_image'
         )
