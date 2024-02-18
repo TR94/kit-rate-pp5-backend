fa6c29b17d94df576222c945408335bc7eaab998
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-18 14:55:40 +0000

    Revert previous change - products > models.py ordering

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-created_at']
 
     def __str__(self):
         return f'{self.title}'
